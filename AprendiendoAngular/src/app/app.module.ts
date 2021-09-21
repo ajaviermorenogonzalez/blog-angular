a29a3a81720a3bc69e14e9d6f34583b4917589b2
ajaviermorenogonzalez <ajaviermorenogonzalez@gmail.com>
2021-09-21 18:52:05 +0200

    Routing Angular

--- AprendiendoAngular/src/app/app.module.ts
+++ AprendiendoAngular/src/app/app.module.ts
@@ -6,6 +6,12 @@ import { HeaderComponent } from './components/header/header.component';
 import { SliderComponent } from './components/slider/slider.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { PaginaComponent } from './components/pagina/pagina.component';
+import { routing, appRoutingProviders } from './app.routing';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
@@ -14,12 +20,18 @@ import { FooterComponent } from './components/footer/footer.component';
     SliderComponent,
     SidebarComponent,
     FooterComponent,
+    HomeComponent,
+    BlogComponent,
+    FormularioComponent,
+    PaginaComponent,
+    ErrorComponent,
     
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
-  providers: [],
+  providers: [appRoutingProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ AprendiendoAngular/src/app/app.routing.ts
@@ -0,0 +1,27 @@
+//Importar los modulos del router de angular
+import { ModuleWithProviders } from "@angular/core";
+import {Routes, RouterModule} from "@angular/router";
+
+//Importar componentes a los cuales ls quiero hacer una pagina exclusiva
+
+import { HomeComponent } from "./components/home/home.component";
+import { BlogComponent } from "./components/blog/blog.component";
+import { FormularioComponent } from "./components/formulario/formulario.component";
+import { PaginaComponent } from "./components/pagina/pagina.component";
+import { ErrorComponent } from "./components/error/error.component";
+//Array de rutas
+
+const appRoutes : Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'blog', component: BlogComponent},
+    {path: 'formulario', component: FormularioComponent},
+    {path: 'pagina-de-pruebas', component: PaginaComponent},
+    {path: '**', component: ErrorComponent} // La ruta ** debe ir la última porque si no las otras no cargan
+
+];
+
+// Exportar el modulo de rutas
+
+export const appRoutingProviders: any[] = [];   //Crea un array vacio sin tipo predefinido 
+export const routing : ModuleWithProviders<any> = RouterModule.forRoot(appRoutes);  // Carga todas las routas de appRoutes, hace que "existan" en Angular 
